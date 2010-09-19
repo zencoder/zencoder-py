99c8dac152a23fea9d4394ae62eb561e937a9c22
Alex Schworer <alex.schworer@gmail.com>
2010-09-19 15:23:46 -0700

    add test for setting the api_key with the default arg

--- test/test_zencoder.py
+++ test/test_zencoder.py
@@ -1,9 +1,16 @@
 import unittest
+from zencoder import Zencoder
 
 class TestZencoder(unittest.TestCase):
     def setUp(self):
         """ Initialize Zencoder for testing """
         pass
+
+    def test_api_key(self):
+        """ initialize zencoder object and test api key """
+        api_key = 'abcd123'
+        zc = Zencoder(api_key=api_key)
+        self.assertEquals(zc.api_key, api_key)
 
 if __name__ == "__main__":
     unittest.main()
