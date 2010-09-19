78a490fe092c495e3fda3b35a87122a0755e41fb
Alex Schworer <alex.schworer@gmail.com>
2010-09-19 15:18:07 -0700

    set the api_key correctly will fall back on grabbing the ZENCODER_API_KEY env var

--- zencoder/zencoder.py
+++ zencoder/zencoder.py
@@ -1,10 +1,22 @@
 """
 Main Zencoder module
 """
+import os
+
+class ZencoderError(Exception):
+    pass
 
 class Zencoder(object):
     base_url = 'https://app.zencoder.com/api'
-    def __init__(self, api_key):
+
+    def __init__(self, api_key=None):
         """ Initialize Zencoder """
-        self.api_key = api_key
+        if not api_key:
+            try:
+                self.api_key = os.environ['ZENCODER_API_KEY']
+            except KeyError:
+                raise ZencoderError('ZENCODER_API_KEY not set')
+        else:
+            self.api_key = api_key
+
 
