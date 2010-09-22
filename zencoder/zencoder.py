abdc3a4f115cfec56dfe8de7aec91a6a8351f996
Alex Schworer <alex.schworer@gmail.com>
2010-09-21 22:30:58 -0700

    add imports and as_xml kwarg

--- zencoder/zencoder.py
+++ zencoder/zencoder.py
@@ -1,16 +1,19 @@
 """
 Main Zencoder module
 """
+
 import os
+import json
+import httplib2
 
 class ZencoderError(Exception):
     pass
 
 class Zencoder(object):
     """ Main class for pushing jobs to zencoder """
-    def __init__(self, api_key=None):
+    def __init__(self, api_key=None, as_xml=False):
         """ Initialize Zencoder """
-        self.base_url = 'https://app.zencoder.com/api'
+        self.base_url = 'https://app.zencoder.com/api/'
         if not api_key:
             try:
                 self.api_key = os.environ['ZENCODER_API_KEY']
