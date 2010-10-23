1f16c8d71ef550c92f3f1851a8b1a94285675eb2
Alex Schworer <alex.schworer@gmail.com>
2010-10-23 16:58:32 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name='zencoder',
+      version='0.1',
+      description='Integration library for Zencoder',
+      author='Alex Schworer',
+      author_email='alex.schworer@gmail.com',
+      url='http://github.com/schworer/zencoder-py',
+      license="MIT License",
+      packages=['zencoder']
+     )
+
