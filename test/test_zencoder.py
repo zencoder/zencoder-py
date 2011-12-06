9e1f6baa8f3fb704b0cdd4dc163533f34178b4eb
Alex Schworer <alex.schworer@gmail.com>
2011-12-06 08:35:41 -0800

    add tests for setting the api version

--- test/test_zencoder.py
+++ test/test_zencoder.py
@@ -9,7 +9,7 @@ class TestZencoder(unittest.TestCase):
 
     def test_api_key(self):
         """ initialize zencoder object and test api key """
-        api_key = 'abcd123'
+        api_key = 'testapikey'
         zc = Zencoder(api_key=api_key)
         self.assertEquals(zc.api_key, api_key)
 
@@ -18,6 +18,21 @@ class TestZencoder(unittest.TestCase):
         os.environ['ZENCODER_API_KEY'] = 'abcd123'
         zc = Zencoder()
         self.assertEquals(zc.api_key, 'abcd123')
+
+    def test_default_api_version(self):
+        os.environ['ZENCODER_API_KEY'] = 'abcd123'
+        zc = Zencoder()
+        self.assertEquals(zc.base_url, 'https://app.zencoder.com/api/v2/')
+
+    def test_set_api_version(self):
+        os.environ['ZENCODER_API_KEY'] = 'abcd123'
+        zc = Zencoder(api_version='v1')
+        self.assertEquals(zc.base_url, 'https://app.zencoder.com/api/v1/')
+
+    def test_set_api_edge_version(self):
+        os.environ['ZENCODER_API_KEY'] = 'abcd123'
+        zc = Zencoder(api_version='edge')
+        self.assertEquals(zc.base_url, 'https://app.zencoder.com/api/')
 
 if __name__ == "__main__":
     unittest.main()
