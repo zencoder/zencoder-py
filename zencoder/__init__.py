e65f7b3afe66f38bcc884ecf7196050839b7c240
Alex Schworer <aschworer@brightcove.com>
2013-05-03 16:01:12 -0700

    add version, author and title attrs to zencoder package

--- zencoder/__init__.py
+++ zencoder/__init__.py
@@ -1,4 +1,9 @@
-from core import Zencoder
-from core import ZencoderResponseError
+from .core import Zencoder
+from .core import ZencoderResponseError
 
-from core import Account
+from .core import Account
+
+__version__ = '0.5.2'
+__title__ = 'zencoder'
+__author__ = 'Alex Schworer'
+
